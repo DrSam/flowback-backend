e3766954bab545a3c09ee5a60dc669ff9e82535f
Salem Salem <salem.m.salem@gmail.com>
2024-07-26 23:32:20 +0300

    Add user fields

--- flowback/user/models.py
+++ flowback/user/models.py
@@ -121,6 +121,13 @@ class User(AbstractBaseUser, PermissionsMixin):
         instance.kanban.delete()
         instance.schedule.delete()
 
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
 
 post_save.connect(User.post_save, sender=User)
 post_delete.connect(User.post_delete, sender=User)
