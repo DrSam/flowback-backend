15c6cb1c3535d48f9f9bc20fa43e0c66154c144f
Salem Salem <salem.m.salem@gmail.com>
2024-10-12 08:52:52 +0300

    Add aspect polls

--- flowback/decidables/filters.py
+++ flowback/decidables/filters.py
@@ -5,6 +5,14 @@ from flowback.decidables import models as decidable_models
 class DecidableFilter(django_filters.FilterSet):
     title_contains = django_filters.CharFilter(lookup_expr='icontains',field_name='title')
     title_startswith = django_filters.CharFilter(lookup_expr='istartswith',field_name='title')
+    is_root = django_filters.BooleanFilter(method='filter_is_root')
+
+    def filter_is_root(self, queryset, name, value):
+        if value:
+            queryset = queryset.filter(
+                root_decidable__isnull=True
+            )
+        return queryset
 
     class Meta:
         model = decidable_models.Decidable
