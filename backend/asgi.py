9817895a7edd8afebd12b48e93ecdd1de7864ddb
Salem Salem <salem.m.salem@gmail.com>
2024-11-28 14:01:44 +0300

    Update asgi connection

--- backend/asgi.py
+++ backend/asgi.py
@@ -9,19 +9,20 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from flowback.chat import urls
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 
 from backend.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AllowedHostsOriginValidator(
         TokenAuthMiddleware(
             URLRouter(
